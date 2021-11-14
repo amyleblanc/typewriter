d4f1e721eeeb222964e9b133256b7f58ad7877ab
Amy <amymccarthy2014@gmail.com>
2021-11-14 06:50:26 +0000

    include comments on code logic

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,11 @@
 const sentence = 'hello there from lighthouse labs';
 
-for (let i = 0; i < sentence.length; i++) { // this is animated, needs newline
+for (let i = 0; i < sentence.length; i++) { // loop through sentence to access each character
   setTimeout (() => {
-    process.stdout.write(sentence[i]);
-  }, i * 50);
+    process.stdout.write(sentence[i]); // print each character with a timeout...
+  }, i * 50); // ...of 50 ms per character
 };
 
-setTimeout(() => {
+setTimeout(() => { // apply a second timeout to print ' ' to the console, which forces the next command to move to a new line
   console.log(' ');
 }, sentence.length * 50);
